d07b9c908053faf75bf95d18957a6840a897b7bb
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-19 16:15:33 +0800

    Auto-commit: Project implementation in progress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  eslint.defaults,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
     plugins: {
