cc6cd208bac6a8f4e3f2a96207b788da2715879d
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-19 16:15:59 +0800

    Auto-commit: Project implementation in progress

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
   testMatch: ['**/tests/**/*.test.ts'],
   collectCoverage: true,
